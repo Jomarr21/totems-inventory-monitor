1456c6ee4105f6029b7e606bb50262ddcdffc528
Jomarr21 <jomarr0121@gmail.com>
2026-02-24 15:13:51 -0500

    [feat]: Remove React folder and update configuration files and middlewares
    - Remove complete React implementation (components, configurations, and dependencies)
    - Update manifest.json, whitelabels clients, and inventory middlewares
    - Remove related store interfaces

--- node/clients/whitelabels/index.ts
+++ node/clients/whitelabels/index.ts
@@ -1,4 +1,4 @@
-import type { InstanceOptions, IOContext, IOResponse } from '@vtex/api'
+import type { InstanceOptions, IOContext } from '@vtex/api'
 import { ExternalClient } from '@vtex/api'
 import { getEnvironmentConfig } from '../../config'
 
@@ -6,18 +6,25 @@ export class WhiteLabelsClient extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
     const envConfig = getEnvironmentConfig(context?.production)
 
-    super('', context, {...options,
-    headers: {
-      'X-VTEX-Use-Https': 'true',
-      'X-VTEX-API-AppKey': envConfig.apiKey,
-      "X-VTEX-API-AppToken": envConfig.apiToken,
-      "Content-Type":"application/json",
-      "Accept":"application/json"
-    }}
-      );
+    super('', context, {
+      ...options,
+      headers: {
+        'X-VTEX-Use-Https': 'true',
+        'X-VTEX-API-AppKey': envConfig.apiKey,
+        'X-VTEX-API-AppToken': envConfig.apiToken,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
   }
 
-  public getInventory = async (wl: string, sku: string, warehouseId: string): Promise<IOResponse<string>> => {
-    return  this.http.getRaw(`https://${wl}.vtexcommercestable.com.br/api/logistics/pvt/inventory/items/${sku}/warehouses/${warehouseId}/dispatched`, {});
-  };
+  public getSkuWarehouses = async (wl: string, sku: string) => {
+    const url = `https://${wl}.vtexcommercestable.com.br/api/logistics/pvt/inventory/skus/${sku}`
+    return this.http.get<any>(url)
+  }
+
+  public getInventoryByWarehouse = async (wl: string, sku: string, warehouseId: string) => {
+    const url = `https://${wl}.vtexcommercestable.com.br/api/logistics/pvt/inventory/items/${sku}/warehouses/${warehouseId}`
+    return this.http.get<any>(url)
+  }
 }
--- node/middlewares/inventory.ts
+++ node/middlewares/inventory.ts
@@ -1,22 +1,59 @@
-const wls = ['jumbocolombiaioswl108', 'jumbocolombiaioswl11', 'jumbocolombiaioswl126', 'jumbocolombiaioswl128', 'jumbocolombiaioswl13', 'jumbocolombiaioswl14', 'jumbocolombiaioswl148', 'jumbocolombiaioswl15', 'jumbocolombiaioswl16', 'jumbocolombiaioswl167', 'jumbocolombiaioswl17', 'jumbocolombiaioswl18', 'jumbocolombiaioswl185',
-              'jumbocolombiaioswl19', 'jumbocolombiaioswl194', 'jumbocolombiaioswl22', 'jumbocolombiaioswl23', 'jumbocolombiaioswl24', 'jumbocolombiaioswl27', 'jumbocolombiaioswl31', 'jumbocolombiaioswl36', 'jumbocolombiaioswl43', 'jumbocolombiaioswl44', 'jumbocolombiaioswl447', 'jumbocolombiaioswl45', 'jumbocolombiaioswl47', 'jumbocolombiaioswl48',
-              'jumbocolombiaioswl70', 'jumbocolombiaioswl76', 'jumbocolombiaioswl83', 'jumbocolombiaioswl84', 'jumbocolombiaioswl87', 'jumbocolombiaioswl9', 'metrocolombiaioswl10', 'metrocolombiaioswl124', 'metrocolombiaioswl166', 'metrocolombiaioswl171', 'metrocolombiaioswl18', 'metrocolombiaioswl183', 'metrocolombiaioswl195', 'metrocolombiaioswl203',
-              'metrocolombiaioswl25', 'metrocolombiaioswl26', 'metrocolombiaioswl28', 'metrocolombiaioswl32', 'metrocolombiaioswl33', 'metrocolombiaioswl34', 'metrocolombiaioswl35', 'metrocolombiaioswl38', 'metrocolombiaioswl39', 'metrocolombiaioswl40', 'metrocolombiaioswl42', 'metrocolombiaioswl49', 'metrocolombiaioswl71', 'metrocolombiaioswl73',
-              'metrocolombiaioswl74', 'metrocolombiaioswl75', 'metrocolombiaioswl77', 'metrocolombiaioswl78', 'metrocolombiaioswl79', 'metrocolombiaioswl80', 'metrocolombiaioswl81', 'metrocolombiaioswl85', 'metrocolombiaioswl97', 'metrocolombiaioswl82']
-
-// TO DO: Eliminar WLS no necesarios y agregar los que falten. Hacer lo mismo en el Manifest
+const wls = ['jumbocolombiaretiroentiendahayuelos'] // TO DO: Agregar los WL que falten. Hacer lo mismo en el Manifest
 
 export async function inventory(ctx: Context) {
-  const {clients: { whiteLabelsClient }, url}= ctx
+  const {
+    clients: { whiteLabelsClient },
+    url,
+  } = ctx
+
   const sku = url.substring(url.lastIndexOf('/') + 1)
 
-  const response = await Promise.all(wls.map(async url => {
-    const warehouseId = url.replace(/\D/g, "")
-    const resp = await whiteLabelsClient.getInventory(url,sku, warehouseId);
-    return {whitelabel: url, wlUrl:`https://${url}.myvtex.com/admin`,info: resp.data}
-  }));
+  const response = await Promise.all(
+    wls.map(async (wl) => {
+      try {
+        const skusResp = await whiteLabelsClient.getSkuWarehouses(wl, sku)
+        const balances: Array<{ warehouseId: string }> = skusResp?.balance || []
 
-  console.log(response, wls);
+        const inventories = await Promise.all(
+          balances.map(async ({ warehouseId }) => {
+            try {
+              const invResp = await whiteLabelsClient.getInventoryByWarehouse(wl, sku, warehouseId)
+
+              return invResp.map((inv: any) => ({
+                whitelabel: wl,
+                wlUrl: `https://${wl}.myvtex.com/admin`,
+                warehouseId,
+                totalQuantity: inv?.totalQuantity ?? null,
+                reservedQuantity: inv?.reservedQuantity ?? null,
+                availableQuantity: inv?.availableQuantity ?? null,
+              }))
+            } catch (err: any) {
+              return [
+                {
+                  whitelabel: wl,
+                  wlUrl: `https://${wl}.myvtex.com/admin`,
+                  warehouseId,
+                  error:
+                    err?.message ?? 'Error fetching warehouse inventory',
+                },
+              ]
+            }
+          })
+        )
+
+        return inventories.flat()
+      } catch (err: any) {
+        return [
+          {
+            whitelabel: wl,
+            wlUrl: `https://${wl}.myvtex.com/admin`,
+            error: err?.message ?? 'Error fetching WL sku list',
+          },
+        ]
+      }
+    })
+  )
+
   ctx.status = 200
-  ctx.body = response
+  ctx.body = response.flat()
 }
